35c9cbcf9ac3418e63465dd0700810fd36d04df9
Xedlefix <xedlefix@gmail.com>
2020-10-16 22:15:38 +0200

    Part 2

--- SorrowServer.cs
+++ SorrowServer.cs
@@ -1,6 +1,58 @@
-﻿namespace SorrowServer
+﻿using System;
+using System.IO;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace SorrowServer
 {
-    public class Class1
+    public class SorrowServer : TerrariaPlugin
     {
+        public override string Name => "SorrowServer";
+        public override string Description => "Not important plugin at all";
+        public override string Author => "Xedlefix";
+        public override Version Version => new Version("1.0.0");
+
+        public SorrowServer(Main game) : base(game)
+        {
+            Order = 1;
+            
+        }
+
+        public override void Initialize()
+        {
+            TShockAPI.GetDataHandlers.ChestOpen.Register(ChestOpen);
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData, -10);
+        }
+
+        
+        
+        private void OnGetData(GetDataEventArgs args)
+        {
+            if (args.Handled)
+                return;
+
+            if (args.MsgID == PacketTypes.SpawnBossorInvasion)
+            {
+                using (BinaryReader reader = new BinaryReader(new MemoryStream(args.Msg.readBuffer, args.Index, args.Length)))
+                {
+                    var tSPlayer = TShock.Players[reader.ReadInt16()];
+                    var npcid = reader.ReadInt16();
+                    
+                    if ()
+                    
+                }
+            }
+        }
+        
+
+        //Chest protection on protected regions
+        private void ChestOpen(object sender, GetDataHandlers.ChestOpenEventArgs e)
+        {
+            if (e.Handled)
+                return;
+
+            e.Handled = !e.Player.HasBuildPermission(e.X, e.Y);
+        }
     }
 }
