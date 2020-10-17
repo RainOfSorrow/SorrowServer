fa237827331a00cbc5ced515d0552379a3cfa476
Xedlefix <xedlefix@gmail.com>
2020-10-17 03:13:26 +0200

    Made some steps into npcblocking system.

--- SorrowServer.cs
+++ SorrowServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
@@ -13,6 +14,9 @@ namespace SorrowServer
         public override string Author => "Xedlefix";
         public override Version Version => new Version("1.0.0");
 
+        
+        public Config config = new Config();
+        
         public SorrowServer(Main game) : base(game)
         {
             Order = 1;
@@ -21,12 +25,51 @@ namespace SorrowServer
 
         public override void Initialize()
         {
+            Commands.ChatCommands.Add(new Command("sorrow.npcblocker", NpcBlockerCommand, "npcblocker", "nb"));
+            
             TShockAPI.GetDataHandlers.ChestOpen.Register(ChestOpen);
             ServerApi.Hooks.NetGetData.Register(this, OnGetData, -10);
+
+            config = Config.Read("config.json");
+            config.Write("config.json");
         }
 
-        
-        
+
+        private void NpcBlockerCommand(CommandArgs args)
+        {
+            if (args.Parameters.Count > 1)
+                switch (args.Parameters[0])
+                {
+                    case "allow":
+                    {
+                        int id = 0;
+
+                        if (int.TryParse(args.Parameters[1], out id))
+                        {
+                            if (config.BlockedNPCs.Contains(id))
+                                
+                            
+                            return;
+                        }
+                        
+                        
+                        
+                        
+                        return;
+                    }
+                    case "deny":
+                    {
+                        
+                        return;
+                    }
+                }
+
+            args.Player.SendInfoMessage("NpcBlocker:");
+            args.Player.SendSuccessMessage("- /nb allow <name/id>");
+            args.Player.SendSuccessMessage("- /nb deny <name/id>");
+        }
+
+
         private void OnGetData(GetDataEventArgs args)
         {
             if (args.Handled)
@@ -38,8 +81,12 @@ namespace SorrowServer
                 {
                     var tSPlayer = TShock.Players[reader.ReadInt16()];
                     var npcid = reader.ReadInt16();
-                    
-                    if ()
+
+                    if (config.BlockedNPCs.Contains(npcid))
+                    {
+                        args.Handled = true;
+                        tSPlayer.SendErrorMessage("Ten mob jest aktualnie niedostepny.");
+                    }
                     
                 }
             }
